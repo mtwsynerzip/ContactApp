737b8aa945409fb5746f363e478150c72c4bb228
Mike <mike.watson@synerzip.com>
2021-01-05 16:36:34 -0600

    Test change

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 var fs = require('fs');
 var path = require('path');
 
-exports.get = function(event, context, callback) {
+exports.get = (event, context, callback) => {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
+
+
   var result = {
     statusCode: 200,
     body: contents.toString(),
-    headers: {'content-type': 'text/html'}
+    headers: { 'content-type': 'text/html' }
   };
 
   callback(null, result);
