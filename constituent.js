01f0ac053a2d40473e576d4d72eec408d4e782b9
Mike <mike.watson@synerzip.com>
2021-01-06 13:06:31 -0600

    figuring out node.js

--- constituent.js
+++ constituent.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+exports.get = (event, context, callback) => {
+  var contents = '<HTML><TITLE>YO</TITLE><BODY>Hey</BODY</HTML>';
+
+
+  var result = {
+    statusCode: 200,
+    body: contents.toString(),
+    headers: { 'content-type': 'text/html' }
+  };
+
+  callback(null, result);
+}
